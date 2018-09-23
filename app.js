24a5fe24591c3cc89cafb894dfe2b6abb6643111
TRINH KY NAM <namct@TRINHs-MacBook-Pro.local>
2018-09-23 14:30:08 +0300

    ex5

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 // grab express
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 
 // get objectId
 var ObjectID = mongodb.ObjectID;
 
 // create an express app
 var app = express();
-
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
 app.use(bodyParser.json());
 
-var CONTACTS_COLLECTION = "contacts";
-
-var ig = require("instagram-node").instagram();
-
-// configure instagram app with your access_token
-ig.use({
-  // get access token here: http://instagram.pixelunion.net/
-  access_token: "3665886821.1677ed0.90a90191fed64cdc9bc7440bbc125a46"
-});
+var CONTACTS_COLLECTION = 'contacts';
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -31,42 +20,31 @@ mongodb.MongoClient.connect(
   process.env.MONGODB_URI,
   function(err, database) {
     if (err) {
-      console.log(process.env.MONGODB_URI);
       console.log(err);
       process.exit(1);
     }
 
     // Save database object from the callback for reuse.
     // mongo version ^3.0 will return a client object containing the database object
-    db = database.db("js-based");
-    console.log("Database connection ready");
+    db = database.db('js-based');
+    console.log('Database connection ready');
 
     // Initialize the app.
     var server = app.listen(process.env.PORT || 8080, function() {
       var port = server.address().port;
-      console.log("App now running on port", port);
+      console.log('App now running on port', port);
     });
   }
 );
 
 // home page route - popular images
-app.get("/", function(req, res) {
-  // use the instagram package to get popular media
-  ig.user_self_media_recent(function(
-    err,
-    medias,
-    pagination,
-    remaining,
-    limit
-  ) {
-    // render the home page and pass in the popular images
-    res.render("pages/index", { grams: medias });
-  });
+app.get('/', function(req, res) {
+  console.log('Test');
 });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
+  console.log('ERROR: ' + reason);
   res.status(code || 500).json({ error: message });
 }
 
@@ -75,38 +53,70 @@ function handleError(res, reason, message, code) {
  *    POST: creates a new contact
  */
 
-app.get("/contacts", function(req, res) {});
+app.get('/contacts', function(req, res) {});
 
-app.post("/contacts", function(req, res) {
+app.post('/contacts', function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
   if (!(req.body.firstName || req.body.lastName)) {
     handleError(
       res,
-      "Invalid user input",
-      "Must provide a first or last name.",
+      'Invalid user input',
+      'Must provide a first or last name.',
       400
     );
   }
 
   db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
+      handleError(res, err.message, 'Failed to create new contact.');
     } else {
       res.status(201).json(doc.ops[0]);
     }
   });
 });
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
+app.get('/api/contacts/:id', function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne(
+    { _id: new ObjectID(req.params.id) },
+    function(err, doc) {
+      if (err) {
+        handleError(res, err.message, 'Failed to get contact');
+      } else {
+        res.status(200).json(doc);
+      }
+    }
+  );
+});
 
-app.get("/contacts/:id", function(req, res) {});
+app.put('/api/contacts/:id', function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
 
-app.put("/contacts/:id", function(req, res) {});
+  db.collection(CONTACTS_COLLECTION).updateOne(
+    { _id: new ObjectID(req.params.id) },
+    updateDoc,
+    function(err, doc) {
+      if (err) {
+        handleError(res, err.message, 'Failed to update contact');
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    }
+  );
+});
 
-app.delete("/contacts/:id", function(req, res) {});
+app.delete('/api/contacts/:id', function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne(
+    { _id: new ObjectID(req.params.id) },
+    function(err, result) {
+      if (err) {
+        handleError(res, err.message, 'Failed to delete contact');
+      } else {
+        res.status(200).json(req.params.id);
+      }
+    }
+  );
+});
