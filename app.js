17fb5e43ba015801e1d63555e0be917890f2a4c9
TRINH KY NAM <namct@TRINHs-MacBook-Pro.local>
2018-09-23 23:09:34 +0300

    test

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ var CONTACTS_COLLECTION = 'contacts';
 var app = express();
 app.use(bodyParser.json());
 
-// Create link to Angular build directory
-var distDir = __dirname + '/dist/';
-app.use(express.static(distDir));
-
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+// Connect to the database before starting the application server.
 mongodb.MongoClient.connect(
-  process.env.MONGODB_URI,
-  function(err, database) {
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/test',
+  function(err, client) {
     if (err) {
       console.log(err);
       process.exit(1);
     }
 
     // Save database object from the callback for reuse.
-    // mongo version ^3.0 will return a client object containing the database object
-    db = database.db('js-based');
+    db = client.db();
     console.log('Database connection ready');
 
     // Initialize the app.
@@ -36,10 +32,7 @@ mongodb.MongoClient.connect(
   }
 );
 
-// home page route - popular images
-app.get('/', function(req, res) {
-  console.log('Test');
-});
+// CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -47,34 +40,48 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({ error: message });
 }
 
-/*  "/contacts"
+/*  "/api/contacts"
  *    GET: finds all contacts
  *    POST: creates a new contact
  */
 
-app.get('/api/contacts', function(req, res) {});
+app.get('/api/contacts', function(req, res) {
+  db.collection(CONTACTS_COLLECTION)
+    .find({})
+    .toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, 'Failed to get contacts.');
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+});
 
 app.post('/api/contacts', function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(
-      res,
-      'Invalid user input',
-      'Must provide a first or last name.',
-      400
-    );
+  if (!req.body.name) {
+    handleError(res, 'Invalid user input', 'Must provide a name.', 400);
+  } else {
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(
+      err,
+      doc
+    ) {
+      if (err) {
+        handleError(res, err.message, 'Failed to create new contact.');
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
   }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, 'Failed to create new contact.');
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
 });
+
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
 
 app.get('/api/contacts/:id', function(req, res) {
   db.collection(CONTACTS_COLLECTION).findOne(
